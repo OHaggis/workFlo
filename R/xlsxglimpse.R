9f34ed597eec904aadfd20538095047e479350dc
Flo <50618016+OHaggis@users.noreply.github.com>
2021-03-18 14:23:47 +0100

    Update xlsxglimpse.R

--- R/xlsxglimpse.R
+++ R/xlsxglimpse.R
@@ -32,9 +32,6 @@
 #'   xlsxglimpse()
 xlsxglimpse <- function(df) {
   
-  # check type
-  # if(!(is_tibble(df) | is.matrix(df) | is.vector(df) | is.data.frame(df))) stop("Supplied object is of type 'list'. This function works with 'tibble', 'data.frame' and 'matrix' types. Did you mean to specify a subset of the list? e.g. list$object1")
-  
   if(is.matrix(df)) df <- as_tibble(df, rownames = "rownames")
   
   wb <- openxlsx::createWorkbook()
