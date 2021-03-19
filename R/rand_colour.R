f3069ec6b68e4ecd8d153b949ecae83660d4c3c0
Flo <50618016+OHaggis@users.noreply.github.com>
2021-03-19 11:39:25 +0100

    bugfix rand_colour()
    Fixed a bug where the number of returned colours would always be the maximum number.

--- R/rand_colour.R
+++ R/rand_colour.R
@@ -4,13 +4,15 @@
 #' https://stackoverflow.com/questions/15282580/how-to-generate-a-number-of-most-distinctive-colors-in-r
 #' User: JelenaCuklina
 #' 
+#' Max. retrieved different colours: 74
+#' 
 #' @import RColorBrewer
 #' @export
 #' @param n number of colours to return
 #' @examples 
 #' rand_colour(100)
 rand_colour <- function(n = 20) {
-  qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
-  col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
-  return(col_vector)
+  qual_col_pals <-  brewer.pal.info[brewer.pal.info$category == 'qual',]
+  col_vector <-  unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
+  return(sample(col_vector, n))
 }
